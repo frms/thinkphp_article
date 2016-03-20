d905238806adb123867a24d1eaa1ef5187ce78b8
frms <bin@bindeMacBook-Air.local>
2016-03-20 14:53:50 +0800

    前台测试
    前台测试

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,12 +1,15 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+
 class IndexController extends Controller {
-	public function index(){
-		
+	public function index() {
+
+		$type = M('type');
+		$this->assign('type', $type);
+
 		$this->display();
-		
+
 	}
 
 }
-
