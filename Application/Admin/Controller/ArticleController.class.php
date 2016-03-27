563e95a93c9fd18e717b51b6c5e3f66924eedaeb
frms <740561104@qq.com>
2016-03-27 16:21:38 +0800

    多选
    多选

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -132,4 +132,32 @@ class ArticleController extends BaseController {
 		}
 	}
 
+	public function deletes() {
+		$getid = I('aiticleid'); //获取选择的复选框的值
+		if (!$getid) {
+			$this->error('未选择记录');
+		} //没选择就提示信息
+		//$getids = implode(',', $getid); //选择一个以上，就用,把值连接起来(1,2,3)这样
+		//$id = is_array($getid) ? $getids : $getid; //如果是数组，就把用,连接起来的值覆给$id,否则就覆获取到的没有,号连接起来的值
+		if (is_array($getid)) {
+			$where = 'id in(' . implode(',', $getid) . ')';
+		} else {
+			$where = 'id=' . $getid;
+		}
+		//最后进行数据操作,例如你的是ArticleModel
+		$Result = D("Article")->where($where)->delete();
+		$say = '删除成功';
+		if ($Result === false) {
+			$this->error('操作失败');
+		} else {
+			$this->assign('jumpUrl', U('Article/showlist'));
+			$this->success($say);
+		}
+
+	}
+
+	public function moves() {
+
+	}
+
 }
