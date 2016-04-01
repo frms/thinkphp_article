57946618e4464f9f926ccbdf9aaff1d58b9c891f
zhang bin <740561104@qq.com>
2016-04-01 23:49:06 +0800

    delete nouse
    delete nouse

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,9 +2,9 @@
 return array(
 	//'配置项'=>'配置值'
 	'URL_MODEL' => 2,
-
 	//路由定义
 	'MODULE_ALLOW_LIST' => array('Home', 'manager', 'User'),
 	'DEFAULT_MODULE' => 'Home',
 	'URL_MODULE_MAP' => array('manager' => 'admin'),
+	'ERROR_PAGE' => '/404.html',
 );
